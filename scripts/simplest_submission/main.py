291ee21e75d5c01e114ba2e2f8d2d6d1a778d1d7
Vinitra Swamy <vinitras@gmail.com>
2021-04-07 17:36:59 +0200

    simplest submission with reprozip

--- scripts/simplest_submission/main.py
+++ scripts/simplest_submission/main.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import sys
-#
-# x_file = None
-#
-# if len(sys.argv) > 1:
-#     x_file = sys.argv[1]
 
-x1 = pd.read_csv('X1.csv')
-x2 = pd.read_csv('X2.csv')
+x_files = ['X2.csv', 'X3.csv', 'X4.csv']
+data = []
+for file in x_files:
+    # x1 = pd.read_csv('X1.csv')
+    x = pd.read_csv(file)
+    datapoint = [x.instance_id[0], x.instance_id[1]]
+    data.append(datapoint)
 
-pairs_df = pd.DataFrame(data=[], columns=['left_instance_id', 'right_instance_id'])
+print(data)
+pairs_df = pd.DataFrame(data=data, columns=['left_instance_id', 'right_instance_id'])
 pairs_df.to_csv('output.csv', index=False)
