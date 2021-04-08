2abaf992d8021d10c7c5438cd4619b788b954ae7
Vinitra Swamy <vinitras@gmail.com>
2021-04-08 10:59:38 +0200

    adding very simple X4 blocker

--- src/blocking.py
+++ src/blocking.py
@@ -89,3 +89,40 @@ class X2Blocker(Blocker):
             if b in text:
                 return b
         return 'unknown'
+
+class X4Blocker(Blocker):
+
+    def __init__(self):
+        super().__init__()
+        self.df = None
+
+    def fit(self, data=None):
+        """
+
+        :param data:
+        :return:
+        """
+        # aliasing as df to be precise
+        df = data
+
+        df.fillna('', inplace=True)
+        df = df.applymap(lambda x: str(x).lower().strip())
+
+        pattern_string = '( - )|,|:|\(|\)'
+        df["blocking_key"] = df["brand"] + ' ' + df["size"]
+
+        # Store reference
+        self.df = df
+
+    def transform(self):
+        """
+
+        :return:
+        """
+        grps = self.df.groupby(self.df["blocking_key"]).groups
+        self.keys = grps
+
+        ls = []
+        for g in grps:
+            ls.append(list(grps[g].map(lambda n: self.df["instance_id"][n])))
+        return ls, self.df
