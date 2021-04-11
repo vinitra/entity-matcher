240b9d801b10161ceaee66f12335586b2b5ee06d
Angelika Romanou <agromanou@gmail.com>
2021-04-11 16:15:10 +0200

    Changes main to use the best model

--- src/main.py
+++ src/main.py
@@ -7,13 +7,11 @@ from pipeline import main
 
 if __name__ == '__main__':
     dataset_ids = [1, 2, 3, 4]
-    cluster_num = 2
-    c_method = 'kmeans'
+    c_method = 'cosine'
     encoding = 'use'
 
     main(dataset_ids,
          clustering_method=c_method,
-         cluster_num=cluster_num,
          encoding=encoding,
          evaluate=False,
          store=True)
